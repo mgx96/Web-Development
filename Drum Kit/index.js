957bcb63907ce197e9477c3c50b5ebcd6f93ad7d
Malek Sharabi <m.malek1996@hotmail.com>
2024-01-11 20:31:28 +0100

    Drum Kit (HTML + CSS + JavaScript)

---
+++ Drum Kit/index.js
@@ -0,0 +1,8 @@
+
+var audio = new Audio('./sounds/tom-1.mp3');
+
+for (var i = 0; i < document.querySelectorAll('.drum').length; i++){
+    document.querySelectorAll('.drum')[i].addEventListener("click", function(){
+        this.style.color = "white";
+    });
+}
